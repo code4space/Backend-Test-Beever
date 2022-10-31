57ebbccbd8552afbaa58f3252f6d4d1111422e98
William <mauritius.william.wijaya1@gmail.com>
2022-10-31 17:44:36 +0700

    complete logic test

---
+++ logic_tes/asterix.js
@@ -0,0 +1,15 @@
+let words = "beever";
+
+// Buatlah skema logika untuk memuat kata diatas menjadi berbentuk seperti berikut : 
+// b
+// be
+// bee
+// beev
+// beeve
+// beever
+
+let newWords = ''
+for (let i = 0; i < words.length; i++) {
+    newWords += words[i]
+    console.log(newWords) 
+}
